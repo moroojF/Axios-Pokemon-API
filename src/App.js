808f4c7c29d455d2f0e1b66bf5366e7e020051f1
moroojF <morooj.f2020@gmail.com>
2020-12-01 13:37:00 +0300

    remove unused function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [myPkmns, setMyPkmns] = useState([]);
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/?limit=807')
+      .then(response => {
+        setMyPkmns(response.results);
+      }).catch(err => {
+        console.log(err);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="nav justify-content-center py-3 bg-warning text-muted">
+        <h2>Welcome to Axios Pokemon API</h2>
+      </nav>
+      <div className="container">
+        <div className="row">
+          <div className="col-4"></div>
+          <div className="col-4">
+            <div className="card mt-4">
+              <h5 className="card-header text-muted">View the list</h5>
+              <div className="card-body">
+                <p className="card-text text-muted">Click the button below to show all the Pokemons</p>
+                <button className="btn btn-outline-secondary" onClick={useEffect}>Fetch the Pokemon</button></div>
+            </div>
+          </div>
+          <div className="col-4"></div>
+        </div>
+        <div className="row mt-5">
+          <div className="col-4"></div>
+          <div className="col-4" ><ul>{
+            myPkmns.map((item, i) =>
+              <li key={i}>{item.name}</li>
+            )
+          }</ul></div>
+          <div className="col-4"></div>
+
+        </div>
+
+      </div>
+    </>
   );
 }
 
 export default App;
+
